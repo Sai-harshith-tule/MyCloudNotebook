332c8c65ab5c5b9c880e8fb117ffc15162cbff9f
Sai-harshith-tule <21211a3261@bvrit.ac.in>
2024-06-07 22:06:39 +0530

    Video 56 completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Harshith this is your react aapplication</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={
+            <Home />
+          } />
+          <Route path="/about" element={
+            <About />
+          } />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
